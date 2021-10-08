729d2f268bef6358c4456ae5388120b0ef09d247
Tariq Daouda <tariq.daouda@umontreal.ca>
2021-10-07 23:09:33 -0400

    works needs json

--- demos/models.py
+++ demos/models.py
@@ -17,49 +17,48 @@ def _get_attr(obj, attr_name, human_err_message):
 class DataMaster:
     def __init__(self, dataset_name, mininatch_size, num_workers=8):
     
-    self.mininatch_size = mininatch_size
-    self.num_workers = num_workers
+        self.mininatch_size = mininatch_size
+        self.num_workers = num_workers
 
-    self.trainset = None
-    self.trainloader = None
-    self.testset = None
-    self.testloader = None
-    self.classes = None
-    self.in_size = None
-    self.out_size = None
-    
-    fct = _get_attr(self, "load_"+ dataset_name, "There's no dataset by the name: {attr_name}")
-    fct()
+        self.trainset = None
+        self.trainloader = None
+        self.testset = None
+        self.testloader = None
+        self.classes = None
+        self.in_size = None
+        self.out_size = None
+        
+        fct = _get_attr(self, "load_"+ dataset_name.lower(), "There's no dataset by the name: {attr_name}")
+        fct()
 
-    def load_cifar10():
+    def load_cifar10(self):
         transform = transforms.Compose(
             [
                 transforms.ToTensor(),
-                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
             ]
         )
 
-        self.trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform),
-        self.trainloader = torch.utils.data.DataLoader(trainset, batch_size=self.mininatch_size, shuffle=True, num_workers=self.num_workers),
-        self.testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform),
-        self.testloader = torch.utils.data.DataLoader(testset, batch_size=self.mininatch_size, shuffle=False, num_workers=self.num_workers),
-        self.classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'),
-        self.in_size =  32*32*3,
+        self.trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+        self.trainloader = torch.utils.data.DataLoader(self.trainset, batch_size=self.mininatch_size, shuffle=True, num_workers=self.num_workers)
+        self.testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+        self.testloader = torch.utils.data.DataLoader(self.testset, batch_size=self.mininatch_size, shuffle=False, num_workers=self.num_workers)
+        self.classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+        self.in_size = 32*32*3
         self.out_size =  10
 
-    def load_mnist(mininatch_size, num_workers=8):
+    def load_mnist(self):
         transform = transforms.Compose(
             [
                 transforms.ToTensor(),
             ]
         )
 
-        self.trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform),
-        self.trainloader = torch.utils.data.DataLoader(trainset, batch_size=self.mininatch_size, shuffle=True, num_workers=self.num_workers),
-        self.testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform),
-        self.testloader = torch.utils.data.DataLoader(testset, batch_size=self.mininatch_size, shuffle=False, num_workers=self.num_workers),
-        self.classes = range(10),
-        self.in_size =  28*28,
+        self.trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+        self.trainloader = torch.utils.data.DataLoader(self.trainset, batch_size=self.mininatch_size, shuffle=True, num_workers=self.num_workers)
+        self.testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+        self.testloader = torch.utils.data.DataLoader(self.testset, batch_size=self.mininatch_size, shuffle=False, num_workers=self.num_workers)
+        self.classes = range(10)
+        self.in_size = 28*28
         self.out_size =  10
 
 class MLP(torch.nn.Module):
@@ -69,6 +68,10 @@ class MLP(torch.nn.Module):
     
         fct = _get_attr(self, "_build_" +  build_name.lower(), "There's no build named: {attr_name}")
         fct(**build_kwargs)
+
+        for parameter in self.parameters():
+            parameter.requires_grad_(True)
+            parameter.retain_grad()
 
     def _reset(self):
         self.layers = []
@@ -130,7 +133,7 @@ class MLP(torch.nn.Module):
             add_it = True
             for attr in attrs:
                 try:
-                    obj = getattr(layer, attr)
+                    obj = getattr(obj, attr)
                 except torch.nn.modules.module.ModuleAttributeError:
                     add_it = False
                     break
@@ -162,6 +165,7 @@ class BatchReporter:
         self.variable_name = variable_name
         self.current_tick = 0
         self.dir = None
+        self.mkdir()
 
     def mkdir(self):
         import time
@@ -183,54 +187,103 @@ class BatchReporter:
         fct_name = "get_" +  self.variable_name.lower()
         fct = _get_attr(model, fct_name, "Model has no function: {attr_name}")
 
-        for key, value in fct():
+        for key, value in fct().items():
             path = os.path.join( self.dir, key)
             if not os.path.exists(path):
                 os.mkdir(path)
 
-            filename = os.path.join( path, "%s.npy" % self.tick ) 
-            value = value.cpu().numpy()
+            filename = os.path.join( path, "%s.npy" % self.current_tick ) 
+            value = value.cpu().detach().numpy()
             numpy.save(filename, value)
 
 class Trainer(object):
     """docstring for Trainer"""
-    def __init__(self, loss_name, optimizer_kwargs):
+    def __init__(self, net, loss_name, optimizer_kwargs):
         """loss_name should be in torch.nn, optimizer kwargs is a dict with a field name that must in torcj.optim. The rest must be the parameters for the optimizer"""
         super(Trainer, self).__init__()
+
+        self.net = net
         self.criterion = _get_attr(torch.nn, loss_name, "There's no loss: {attr_name}")()
         
         optimizer_fct = _get_attr(torch.optim, optimizer_kwargs["name"], "There's no optimizer: {attr_name}")
         del optimizer_kwargs["name"]
-        self.optimizer = optimizer_fct(optimizer_kwargs)
+        self.optimizer = optimizer_fct(self.net.parameters(), **optimizer_kwargs)
 
-    def _one_pass(net, data, reporter, training):
+    def _one_pass(self, data, reporter, training):
         self.optimizer.zero_grad()
 
         inputs, labels = data
-        outputs = net(inputs)
+        outputs = self.net(inputs)
         loss = self.criterion(outputs, labels)
         if training:
-            loss.backward()
+            loss.backward(retain_graph = True)
             self.optimizer.step()
-            reporter.tick(net)
+            reporter.tick(self.net)
 
         return loss.item()
 
-
-    def train(self, np_epochs, net, data_master, reporter):
+    def train(self, nb_epochs, data_master, reporter):
         from tqdm import trange
+
+        learnin_curves = {
+            "train": [],
+            "test": []
+        }
 
         pbar = trange(nb_epochs)
         for epoch in pbar:
             train_loss = 0.0
-            for data in data_master.trainloader:
-                self._one_pass(data, reporter=reporter, training=True)
-            
-            for data in data_master.testloader:
-                self._one_pass(data, reporter=None, training=False)
+            for train_batch_id, data in enumerate(data_master.trainloader):
+                train_loss += self._one_pass(data, reporter=reporter, training=True)
+            train_loss = train_loss / (train_batch_id+1)
+            learnin_curves["train"].append(train_loss)
+
+            test_loss = 0.0
+            for test_batch_id, data in enumerate(data_master.testloader):
+                test_loss += self._one_pass(data, reporter=None, training=False)
+            test_loss = test_loss / (test_batch_id+1)
+            learnin_curves["test"].append(test_loss)
                             
             label = "epoch: %d, train: %.4f, test: %.4f" % (epoch, train_loss, test_loss)
             pbar.set_description( label )
+
+        return learnin_curves
+
+def test2():
+    data_master = DataMaster("MNIST", 512, 8)
+    mlp_description = {
+        "layer_description":[
+            {
+                "in_size": data_master.in_size,
+                "out_size": 100,
+                "bias": True,
+                "non_linearity": "ReLU"
+            },
+            {
+                "in_size": 100,
+                "out_size": 100,
+                "bias": True,
+                "non_linearity": "ReLU"
+            },
+            {
+                "in_size": 100,
+                "out_size": 100,
+                "bias": True,
+                "non_linearity": "ReLU"
+            },
+            {
+                "in_size": 100,
+                "out_size": data_master.out_size,
+                "bias": True,
+                "non_linearity": "ReLU"
+            },
+        ]
+    }
+    mlp = MLP("from_list", mlp_description)
+    mlp.initialize("xavier_uniform_")
+    reporter = BatchReporter(2, "weights_gradients")
+    trainer = Trainer(mlp, "CrossEntropyLoss", {"name": "SGD", "lr": 0.001, "momentum":0.9})
+    trainer.train(10, data_master, reporter)
 
 def test1():
     def _i_will_not_write_proper_unit_tests(net):
@@ -278,6 +331,10 @@ def test1():
     }
     mlp = MLP("from_list", mlp_description)
     _i_will_not_write_proper_unit_tests(mlp)
+
+def main():
+    # test1()
+    test2()
 
 if __name__ == '__main__':
     main()
